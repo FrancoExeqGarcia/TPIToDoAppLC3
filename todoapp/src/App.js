f5b4568150fc923e619a88b0e12b9a07cbfe3fe1
mariapazblanco <pazblanco01@gmail.com>
2023-10-26 16:36:59 -0300

    restart repository

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,24 +1,19 @@
 import React from "react";
 import "./App.css";
-import SideBar from "./Components/sideBar/SideBar";
 import User from "./Components/user/User";
 import Projects from "./Components/projects/Projects";
-import AddNewTodo from "./Components/addNewTodo/AddNewTodo";
 import Todos from "./Components/todos/Todos";
 import EditTodo from "./Components/editTodo/EditTodo";
-import NavBar from "./Components/navBar/NavBar";
 
 function App() {
   return (
     <div className="app">
-      <NavBar>
-        <User />
-      </NavBar>
-      <SideBar>
-        <Projects />
-        <AddNewTodo />
-        <EditTodo />
-      </SideBar>
+      <User />
+
+      <Projects />
+
+      <EditTodo />
+
       <Todos />
     </div>
   );
