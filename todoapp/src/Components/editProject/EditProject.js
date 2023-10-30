16a01ea9667716560b9c7740be98c89e6dbef165
Ramiro Di Carlo <ramirodicarlo2@gmail.com>
2023-10-30 09:41:06 -0300

    Update Projects

--- todoapp/src/Components/editProject/EditProject.js
+++ todoapp/src/Components/editProject/EditProject.js
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
 
-const EditProject = () => {
+function EditProject({ project, onUpdateProject, onCancel }) {
+  const [editedName, setEditedName] = useState(project.name);
+  const [editedStartDate, setEditedStartDate] = useState(project.startDate);
+  const [editedEndDate, setEditedEndDate] = useState(project.endDate);
+
+  const handleSaveChanges = () => {
+    const updatedProject = {
+      ...project,
+      name: editedName,
+      startDate: editedStartDate,
+      endDate: editedEndDate,
+    };
+
+    onUpdateProject(updatedProject);
+
+    onCancel();
+  };
+
   return (
-    <div>EditProject</div>
-  )
+    <Form onSubmit={handleSaveChanges}>
+      <Form.Group>
+        <Form.Label>Nombre del Proyecto:</Form.Label>
+        <Form.Control
+          type="text"
+          value={editedName}
+          onChange={(e) => setEditedName(e.target.value)}
+        />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Fecha de Inicio:</Form.Label>
+        <Form.Control
+          type="date"
+          value={editedStartDate}
+          onChange={(e) => setEditedStartDate(e.target.value)}
+        />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Fecha de Finalización:</Form.Label>
+        <Form.Control
+          type="date"
+          value={editedEndDate}
+          onChange={(e) => setEditedEndDate(e.target.value)}
+        />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Guardar
+      </Button>
+      <Button variant="secondary" onClick={onCancel}>
+        Cancelar
+      </Button>
+    </Form>
+  );
 }
 
-export default EditProject
+export default EditProject;
--- todoapp/src/Components/projects/Projects.js
+++ todoapp/src/Components/projects/Projects.js
@@ -1,7 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import ProjectForm from "../addNewProject/AddNewProject";
+import ProjectCard from "../projectCard/ProjectCard";
+import EditProject from "../editProject/EditProject"; // Importa el nuevo componente EditProject
 
-const Projects = () => {
-  return <div>Project</div>;
-};
+function Projects() {
+  const [projects, setProjects] = useState([]);
+  const [editingProjects, setEditingProject] = useState(null);
+
+  const addProject = (newProject) => {
+    setProjects([...projects, newProject]);
+  };
+
+  const deleteProject = (index) => {
+    const updatedProjects = [...projects];
+    updatedProjects.splice(index, 1);
+    setProjects(updatedProjects);
+  };
+
+
+  const editProject = (project) => {
+    if (!project.isCompleted) {
+      setEditingProject(project);
+    }
+  };
+
+  const saveEditedProject= (editedProject) => {
+    const updatedProjects = projects.map((project) =>
+      project.id === editedProject.id ? editedProject : project
+    );
+    setProjects(updatedProjects);
+    setEditingProject(null);
+  };
+
+  const cancelEdit = () => {
+    setEditingProject(null);
+  };
+
+  return (
+    <Container>
+      <h1 className="mt-5">Lista de Tareas</h1>
+
+      <ProjectForm onAddproject={addProject} />
+
+      <Row className="mt-4">
+        {projects.map((project, index) => (
+          <Col key={index} xs={12} md={6} lg={4} className="mb-3">
+            {editingProjects === project ? (
+              <EditProject
+                project={project}
+                onUpdateproject={saveEditedProject}
+                onCancel={cancelEdit}
+              />
+            ) : (
+              <ProjectCard
+                project={project}
+                onDeleteproject={() => deleteProject(index)}
+                onEditproject={editProject}
+              />
+            )}
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+}
 
 export default Projects;
