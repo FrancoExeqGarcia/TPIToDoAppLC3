2fb8fa1d60c784b3991094b8d5d981ed51921cbe
FrancoExeqGarcia <francoexequiel.garcia150@gmail.com>
2023-10-30 12:07:08 -0300

    base editUser

--- todoapp/src/Components/editUser/EditUser.js
+++ todoapp/src/Components/editUser/EditUser.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './EditUser.css'; 
 
 const EditUser = () => {
-  return (
-    <div>EditUser</div>
-  )
-}
+  const [editField, setEditField] = useState(null);
 
-export default EditUser
+  const handleFieldSelection = (field) => {
+    setEditField(field);
+  };
+
+  return (
+    <div className="login-container">
+      <div className="login-box">
+        <h2 className="edit-user-title">Edit User</h2>
+        {editField === null ? (
+          <div className="field-selection">
+            <p>Seleccione la opción que desea editar:</p>
+            <button className="signin-button" onClick={() => handleFieldSelection('username')}>
+              Nombre de usuario
+            </button>
+            <button className="signin-button" onClick={() => handleFieldSelection('email')}>
+              Correo
+            </button>
+            <button className="signin-button" onClick={() => handleFieldSelection('password')}>
+              Contraseña
+            </button>
+          </div>
+        ) : (
+          <div className="edit-field">
+            <p>Edit {editField}:</p>
+            <input
+              type={editField === 'password' ? 'password' : 'text'}
+              placeholder={`Enter new ${editField}`}
+              className="input-control"
+            />
+            <button className="signin-button">Guardar cambios</button>
+          </div>
+        )}
+        <br></br>
+         <button className="return-button">Volver al inicio</button>
+      </div>
+    </div>
+  );
+};
+
+export default EditUser;
