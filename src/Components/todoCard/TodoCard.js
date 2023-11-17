487e330157f2bf7c6d92035fbfbc897792c01e78
FrancoExeqGarcia <francoexequiel.garcia150@gmail.com>
2023-11-16 23:47:53 -0300

    eliminacion callback memo

--- src/Components/todoCard/TodoCard.js
+++ src/Components/todoCard/TodoCard.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useMemo } from "react";
+import React from "react";
 import { Card, Button, Form } from "react-bootstrap";
 
 import { TranslateContext } from "../../services/translationContext/translation.context";
@@ -6,60 +6,41 @@ import useTranslation from "../../custom/useTranslation/useTranslation";
 
 function TodoCard({ task, onDeleteTask, onEditTask, onMarkAsCompleted }) {
   const translate = useTranslation();
-  const startDate= useState(task.startDate);
-  const endDate = useState(task.endDate);
 
-  const isValidDate = useCallback((dateString) => {
-    const isValid = !isNaN(new Date(dateString).getTime());
-    return isValid;
-  }, []);
-
-  const formattedStartDate = useMemo(() => {
-    return new Date(startDate).toLocaleDateString();
-  }, [startDate]);
-
-  const formattedEndDate = useMemo(() => {
-    return new Date(endDate).toLocaleDateString();
-  }, [endDate]);
-
-  const handleEditClick = useCallback(() => {
+  const handleEditClick = () => {
     if (!task.completed) {
       if (task.name.trim() === "") {
         alert("El nombre de la tarea no puede estar vacío.");
         return;
       }
-      if (!isValidDate(startDate) || !isValidDate(endDate)) {
-        alert("Formato de fecha no válido. Utilice el formato correcto.");
-        return;
-      }
-      onEditTask({ ...task, startDate, endDate });
+
+      onEditTask(task);
     }
-  }, [task, startDate, endDate, isValidDate, onEditTask]);
+  };
 
   return (
     <Card bg="dark" key="dark" text="light">
       <Card.Body>
         <Card.Title>{task.name}</Card.Title>
-        <Card.Text>{translate("start_date")}: {formattedStartDate}</Card.Text>
-        <Card.Text>{translate("end_date")}: {formattedEndDate}</Card.Text>
+        <Card.Text>{translate("start_date")}: {new Date(task.startDate).toLocaleDateString()}</Card.Text>
+        <Card.Text>{translate("end_date")}: {new Date(task.endDate).toLocaleDateString()}</Card.Text>
         <Form.Check
           type="checkbox"
           label="Completada"
           onClick={() => onMarkAsCompleted(task)}
-        />
-        <Button
-          variant="info"
-          onClick={handleEditClick}
-          disabled={task.completed}
-        >
-          {translate("edit")}
-        </Button>
-        <Button variant="danger" onClick={() => onDeleteTask(task)}>
-          {translate("delete")}
-        </Button>
-      </Card.Body>
-    </Card>
-  );
-}
-
-export default TodoCard;
+          />
+          <Button
+            variant="info"
+            onClick={handleEditClick}
+            disabled={task.completed}
+          >
+            {translate("edit")}
+          </Button>
+          <Button variant="danger" onClick={() => onDeleteTask(task)}>
+            {translate("delete")}
+          </Button>
+        </Card.Body>
+      </Card>
+    );
+  }
+  export default TodoCard;
