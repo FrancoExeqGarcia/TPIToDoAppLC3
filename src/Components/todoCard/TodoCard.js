79842e20866e26167bd7936e18a54205ad6b1ca5
FrancoExeqGarcia <francoexequiel.garcia150@gmail.com>
2023-11-16 23:23:34 -0300

    eliminacion callback use memo

--- src/Components/todoCard/TodoCard.js
+++ src/Components/todoCard/TodoCard.js
@@ -1,47 +1,47 @@
-import React, { useState, useCallback, useMemo } from "react";
+import React from "react";
 import { Card, Button, Form } from "react-bootstrap";
-
-import { TranslateContext } from "../../services/translationContext/translation.context";
 import useTranslation from "../../custom/useTranslation/useTranslation";
 
 function TodoCard({ task, onDeleteTask, onEditTask, onMarkAsCompleted }) {
   const translate = useTranslation();
-  const startDate= useState(task.startDate);
-  const endDate = useState(task.endDate);
 
-  const isValidDate = useCallback((dateString) => {
+  const isValidDate = (dateString) => {
+    // Implementa la lógica de validación de fecha aquí
+    // Puedes usar bibliotecas como moment.js o JavaScript Date Object para validar la fecha
+    // Devuelve true si la fecha es válida, de lo contrario, devuelve false
+
+    // Por ejemplo, podrías usar el objeto Date de JavaScript para la validación básica:
     const isValid = !isNaN(new Date(dateString).getTime());
+
     return isValid;
-  }, []);
+  };
 
-  const formattedStartDate = useMemo(() => {
-    return new Date(startDate).toLocaleDateString();
-  }, [startDate]);
-
-  const formattedEndDate = useMemo(() => {
-    return new Date(endDate).toLocaleDateString();
-  }, [endDate]);
-
-  const handleEditClick = useCallback(() => {
+  const handleEditClick = () => {
     if (!task.completed) {
       if (task.name.trim() === "") {
         alert("El nombre de la tarea no puede estar vacío.");
         return;
       }
-      if (!isValidDate(startDate) || !isValidDate(endDate)) {
-        alert("Formato de fecha no válido. Utilice el formato correcto.");
+      if (!isValidDate(task.startDate)) {
+        alert("La fecha de inicio no es válida.");
         return;
       }
-      onEditTask({ ...task, startDate, endDate });
+  
+      // Validar fecha de fin
+      if (!isValidDate(task.endDate)) {
+        alert("La fecha de fin no es válida.");
+        return;
+      }
+      onEditTask(task);
     }
-  }, [task, startDate, endDate, isValidDate, onEditTask]);
+  };
 
   return (
     <Card bg="dark" key="dark" text="light">
       <Card.Body>
         <Card.Title>{task.name}</Card.Title>
-        <Card.Text>{translate("start_date")}: {formattedStartDate}</Card.Text>
-        <Card.Text>{translate("end_date")}: {formattedEndDate}</Card.Text>
+        <Card.Text>{translate("start_date")}: {new Date(task.startDate).toLocaleDateString()}</Card.Text>
+        <Card.Text>{translate("end_date")}: {new Date(task.endDate).toLocaleDateString()}</Card.Text>
         <Form.Check
           type="checkbox"
           label="Completada"
