904e23a47d828f71e3b674961cdb3777183d4c76
FrancoExeqGarcia <francoexequiel.garcia150@gmail.com>
2023-11-24 13:26:28 -0300

    edit user

--- src/Components/UserManagement/UserManagement.js
+++ src/Components/UserManagement/UserManagement.js
@@ -5,13 +5,13 @@ const UserManagement = () => {
   const translate = useTranslation();
 
   const [users, setUsers] = useState([]);
-  const [newUser, setNewUser] = useState({
+  const [formData, setFormData] = useState({
     email: "",
     password: "",
+    role: "",
   });
   const [editingUser, setEditingUser] = useState(null);
-  const [selectedRole, setSelectedRole] = useState(""); // Nuevo estado para el rol seleccionado
-
+  const [isEditing, setIsEditing] = useState(false); 
   useEffect(() => {
     getUsers();
   }, []);
@@ -28,11 +28,10 @@ const UserManagement = () => {
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
-    setNewUser({ ...newUser, [name]: value });
-  };
-
-  const handleRoleChange = (e) => {
-    setSelectedRole(e.target.value);
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
   };
 
   const handleAddUser = async (e) => {
@@ -44,31 +43,46 @@ const UserManagement = () => {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ ...newUser, role: selectedRole }), // Enviar el rol seleccionado junto con el usuario
+        body: JSON.stringify(formData),
       });
       const data = await response.json();
       setUsers([...users, data]);
-      setNewUser({ email: "", password: "" });
-      setSelectedRole(""); // Resetear el rol seleccionado después de agregar el usuario
+      setFormData({
+        email: "",
+        password: "",
+        role: "",
+      });
     } catch (error) {
       console.error(translate("error_add_user"));
     }
   };
 
-  const handleEditUser = async (editedUser, id) => {
+  const handleEditUser = async (e) => {
+    e.preventDefault();
+
     try {
-      await fetch(`https://task-minder.onrender.com/users/${id}`, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(editedUser),
-      });
+      const response = await fetch(
+        `https://task-minder.onrender.com/users/${editingUser.id}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
+      const data = await response.json();
       const updatedUsers = users.map((user) =>
-        user.id === editedUser.id ? editedUser : user
+        user.id === data.id ? data : user
       );
       setUsers(updatedUsers);
       setEditingUser(null);
+      setFormData({
+        email: "",
+        password: "",
+        role: "",
+      });
+      setIsEditing(false); 
     } catch (error) {
       console.error(translate("error_edit_user"));
     }
@@ -88,10 +102,22 @@ const UserManagement = () => {
 
   const handleEditClick = (user) => {
     setEditingUser(user);
+    setFormData({
+      email: user.email,
+      password: "",
+      role: user.role,
+    });
+    setIsEditing(true); 
   };
 
   const handleCancelEdit = () => {
     setEditingUser(null);
+    setFormData({
+      email: "",
+      password: "",
+      role: "",
+    });
+    setIsEditing(false);
   };
 
   return (
@@ -101,47 +127,59 @@ const UserManagement = () => {
         {users.map((user) => (
           <li key={user.id}>
             {user.email}{" "}
-            <button className="btn btn-sm btn-primary me-2" onClick={() => handleEditClick(user)}>
+            <button
+              className="btn btn-sm btn-primary me-2"
+              onClick={() => handleEditClick(user)}
+            >
               {translate("edit")}
             </button>{" "}
-            <button className="btn btn-sm btn-danger" onClick={() => handleDeleteUser(user.id)}>
+            <button
+              className="btn btn-sm btn-danger"
+              onClick={() => handleDeleteUser(user.id)}
+            >
               {translate("delete")}
             </button>
           </li>
         ))}
       </ul>
-      <h3>{translate("create_admin")}</h3>
-      <form onSubmit={handleAddUser}>
+      <h3>{isEditing ? translate("edit_admin") : translate("create_admin")}</h3>
+      <form onSubmit={isEditing ? handleEditUser : handleAddUser}>
         <div className="mb-3">
-          <label htmlFor="email" className="form-label">{translate("email")}</label>
+          <label htmlFor="email" className="form-label">
+            {translate("email")}
+          </label>
           <input
             type="email"
             className="form-control"
             id="email"
             name="email"
-            value={newUser.email}
+            value={formData.email}
             onChange={handleInputChange}
           />
         </div>
         <div className="mb-3">
-          <label htmlFor="password" className="form-label">{translate("password")}</label>
+          <label htmlFor="password" className="form-label">
+            {translate("password")}
+          </label>
           <input
             type="password"
             className="form-control"
             id="password"
             name="password"
-            value={newUser.password}
+            value={formData.password}
             onChange={handleInputChange}
           />
         </div>
         <div className="mb-3">
-          <label htmlFor="role" className="form-label">{translate("user_role")}</label>
+          <label htmlFor="role" className="form-label">
+            {translate("user_role")}
+          </label>
           <select
             className="form-select"
             id="role"
             name="role"
-            value={selectedRole}
-            onChange={handleRoleChange}
+            value={formData.role}
+            onChange={handleInputChange}
           >
             <option value="">{translate("select_role")}</option>
             <option value="user">{translate("user")}</option>
@@ -149,9 +187,21 @@ const UserManagement = () => {
             <option value="admin">{translate("admin")}</option>
           </select>
         </div>
-        <button type="submit" className="btn btn-primary">{translate("register")}</button>
+        <button type="submit" className="btn btn-primary">
+          {isEditing ? translate("save") : translate("register")}
+        </button>
+        {isEditing && (
+          <button
+            type="button"
+            className="btn btn-secondary ms-2"
+            onClick={handleCancelEdit}
+          >
+            {translate("cancel")}
+          </button>
+        )}
       </form>
-        </div>
-      )}
+    </div>
+  );
+};
 
 export default UserManagement;
